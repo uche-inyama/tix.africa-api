c1f49f122f7f2aa3710ba4bfb8651e1260943e8d
uche-inyama <depoulet@yahoo.com>
2022-01-11 08:17:37 +0100

    add validation test for event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :start_date, :end_date, :time, :address, :theme,
+  :details, :image_data, :event_type, :status, :user_id
 end
