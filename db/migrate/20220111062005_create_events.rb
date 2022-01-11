bdfcae5022b09854df9bbee69bd7f53ab1c20a57
uche-inyama <depoulet@yahoo.com>
2022-01-11 07:45:24 +0100

    implement user & event model

---
+++ db/migrate/20220111062005_create_events.rb
@@ -0,0 +1,18 @@
+class CreateEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :events do |t|
+      t.date :start_date
+      t.date :end_date
+      t.time :time
+      t.text :address
+      t.text :theme
+      t.text :details
+      t.text :image_data
+      t.string :event_type
+      t.string :status
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
